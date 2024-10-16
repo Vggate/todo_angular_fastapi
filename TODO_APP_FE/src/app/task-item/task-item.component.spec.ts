a16591ae4538a0f5376c4dcd56b5d7fe2c2028f8
Duy Tạ <duyta@Duys-MacBook-Pro.local>
2024-10-16 10:45:24 +0700

    refactor: display due date to list

--- TODO_APP_FE/src/app/task-item/task-item.component.spec.ts
+++ TODO_APP_FE/src/app/task-item/task-item.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
 
 import { TaskItemComponent } from './task-item.component';
 
@@ -8,7 +11,12 @@ describe('TaskItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TaskItemComponent]
+      imports: [TaskItemComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([])
+      ]
     })
     .compileComponents();
 
